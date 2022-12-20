a813fdac3cc13e570ad2ed67175bdf989d7b2c4a
Gabriel M Júnior <gmjplayys@gmail.com>
2022-12-21 00:10:43 +0200

    test

---
+++ test/src/main/java/com/gabrielMJr/NumberAnalyst.java
@@ -0,0 +1,262 @@
+package com.gabrielMJr.tools;
+
+import java.util.ArrayList;
+import com.gabrielMJr.tools.StringAnalyst;
+
+class NumberAnalyst {
+
+    //Atributos
+    private static double value;
+    private static String[] values;
+    
+    // StringAnalyst object
+    private static StringAnalyst sAnalyst = new StringAnalyst();
+    
+    // Numbers vector
+    private static int[] numbers = new int[]
+        {
+           1, 2, 3, 4, 5, 6, 7, 8, 9, 0 
+        };
+    
+    // The result in parts
+    private static Long integer;
+    private static Long decimal;
+    private static Long exponential;
+    
+    
+    
+    /*
+     * Normalizar o expoente
+     */
+    protected ArrayList<Long> expNormalizer(Double value) {
+        
+        // Setting the value to the class variable
+        this.value = value;
+        
+        // Converting value to an string list
+        this.values = String.valueOf(value).split("");
+        
+        ArrayList<Long> result = new ArrayList<>();
+        
+        generateInteger();
+        
+        result.add(getInteger());
+        result.add(getDecimal());
+        
+        return result;
+    }
+    
+    private void generateInteger()
+    {
+    
+      // String that contains integer value
+      String integerValues = "";
+      String decimalvalues = "";
+
+      for (int i = 0; i < values.length; i++)
+      {
+        switch(values[i])
+        {
+          case ".":
+            
+            // Getting decimal values
+            for (int j = i; j < values.length; j++)
+            {
+              switch(values[j])
+              {
+                case "E":
+                  break;
+                  
+                default:
+                  decimalvalues += values[j];
+              }
+            }
+            break;
+              
+          default:
+            integerValues += values[i];
+        }
+      }
+      
+      setInteger(Long.valueOf(integerValues));
+      setDecimal(Long.valueOf(decimalvalues));
+      
+    }
+
+    
+
+
+    /* Normaliza valores retornados por ExpNormalizer*/
+    protected String normalize(Long[] values) {
+        String result = values[0] + ".";
+        result += values[2];
+        if (values[1] == 3) {
+            result += " × 10" + upper(Long.valueOf(values[3]));
+        }     
+        return result;
+    }    
+
+   
+
+    // Coloca os números em forma de expoente
+    protected String upper(Long value) {
+        if (value < 0) {
+            value = -value;
+            return "-" + NumberAnalyst.genUpper(value);
+        } else {
+            return genUpper(value);
+        }
+    }
+    
+        // Colocar os valores em minúsculo
+    private static String genUpper(Long valueV) {
+        Long[] values = new List().valueOf(valueV);
+        String result = "";
+
+        for (Long value: values) {
+            if (value == 0) {
+                result += "⁰";
+            } else if (value == 1) {
+                result += "¹";
+            } else if (value == 2) {
+                result += "²";
+            } else if (value == 3) {
+                result += "³";
+            } else if (value == 4) {
+                result += "⁴";
+            } else if (value == 5) {
+                result += "⁵";
+            } else if (value == 6) {
+                result += "⁶";
+            } else if (value == 7) {
+                result += "⁷";
+            } else if (value == 8) {
+                result += "⁸";
+            } else if (value == 9) {
+                result += "⁹";
+            } else {
+                result = null;
+            }
+          }
+          return result;
+        }
+        
+	
+	/* Método que verifica um número value;
+	 *  se value > 0, returna value entre parentesis˜
+	 * senão, retorna n
+	 */
+	//Sobrecarga do tipo double
+	protected String checkValue(double value) {
+		if (value < 0) {
+			return "(" + value + ")"; 
+		} else {
+			return String.valueOf(value);
+		} 
+	}
+	
+	// Sobrecarga do tipo int
+	protected String checkValue(int value) {
+		if (value < 0) {
+			return "(" + value + ")"; 
+		} else {
+			return String.valueOf(value);
+		}
+	}
+	
+	// Sobrecarga do tipo Long
+	protected String checkValue(Long value) {
+		if (value < 0) {
+			return "(" + value + ")"; 
+		} else {
+			return String.valueOf(value);
+		} 
+	}
+	
+	
+	// Get number from vector
+	protected Long getNumberFromVector(String[] vector)
+	{
+        // String number variable
+        String numberString = "";
+  
+        // Get word from words
+        for (String value: vector)
+        {
+            // Check if it's null
+            if(sAnalyst.isNull(value))
+            {
+                // Continue the loop
+                continue;
+            }
+            else
+            {
+                // Get algarism from word.split("")
+                for (String v: value.split(""))
+                {
+                   // Get number from numbers
+                    for (int num:numbers)
+                    {
+                    // Surround with    try catch for errors
+                        try
+                        {
+                            // Convert v to integer and check if its the same as number
+                            if (Integer.valueOf(v) == num)
+                            {
+                                // Check if StringNumber is null
+                                if (sAnalyst.isNull(numberString))
+                                {
+                                                        numberString = v;
+                                }
+                
+                                // Else, append stringNumber
+                                else
+                                { 
+                                    numberString += v;
+                                }
+                            }
+                        }
+                        // If the conversion arent compatible, continue the loop
+                        catch (NumberFormatException nfe)
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+    // Return the result
+    return Long.valueOf(numberString);
+    }
+	
+	// Getter and setters
+	public Long getInteger()
+	{
+		return integer;
+	}
+	
+	private void setInteger(Long integer)
+	{
+		this.integer = integer;
+	}
+	
+	public Long getDecimal()
+	{
+		return decimal;
+	}
+	
+	private void setDecimal(Long decimal)
+	{
+		this.decimal = decimal;
+	}
+	
+	public Long getExponential()
+	{
+		return exponential;
+	}
+	
+	private void setExponential(Long exponential)
+	{
+		this.exponential = exponential;
+	}
+}
---
+++ test/src/main/java/com/gabrielMJr/StringAnalyst.java
@@ -0,0 +1,99 @@
+ package com.gabrielMJr.tools;
+
+class StringAnalyst {
+
+  /* Atributos
+   * Classe que analisa as strings
+   */
+
+  /*
+   * Retorna true se value == null, senão false
+   */
+  protected Boolean isNull(String value) {
+      // Check if its null
+      if (value == null)
+      {
+          return true;
+      }
+      
+    switch (value.replaceAll("\\s", "")) {
+      case "":
+        return true;
+
+      default:
+        return false;
+    }
+  }
+
+  /*
+   * Se value é somente pontos, retorne true
+   * Reutilizado na classe matemática e física
+   * github.com/gabrielmjr/PMath
+   * github.com/gabrielmjr/Physic
+   */
+  protected Boolean isDot(String value) {
+
+    // Método que verifica e retorna um Boolean
+    return this.verifyDots(value);
+  }
+
+  /*
+   * Continuando com o método acima, isDot
+   */
+
+  private Boolean verifyDots(String value) {
+
+    // Check if it's null
+    if (this.isNull(value))
+    {
+        return false;
+    }
+
+    // Contador de pontos
+    int dotI = 0;
+    int index;
+
+    String[] values = value.replaceAll("\\s+", "").split("");
+    index = values.length;
+
+    for (String v : values) {
+      if (v.equals(".")) {
+        dotI++;
+      } else if (this.isNull(v)) {
+        index--;
+      }
+    }
+
+    /*
+     * Verificar se o número de pontos == número de elementos de values
+     * se for igual, retorne true, senão false
+     */
+    if (dotI == index) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Contador de palavras
+  protected int countWords(String value) {
+    String[] words;
+    int counter = 0;
+
+    if (isNull(value)) {
+      return counter;
+
+    } else {
+
+      // Remove spaces
+      value = value.replaceAll("\\s+", "");
+      words = value.split("");
+
+      for (String x : words) {
+        counter++;
+      }
+
+      return counter;
+    }
+  }
+}
